b502360afc7f16a1c3d963cd1c36a67989527c5f
Patrik Sulander <pakki-s@hotmail.com>
2021-10-01 00:22:57 +0300

    category

---
+++ src/main/java/fi/haagahelia/bookstore/domain/Category.java
@@ -0,0 +1,57 @@
+package fi.haagahelia.bookstore.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+	 @Id
+	 @GeneratedValue(strategy=GenerationType.AUTO)
+		private Long categoryid;
+		private String name;
+		
+		
+		
+		private List<Book> books;
+		
+		public Category() {}
+		
+		public Category(String name) {
+			super();
+			this.name = name;
+		}
+		
+		public Long getCategoryid() {
+			return categoryid;
+		}
+		
+		public void setCategoryid(Long categoryid) {
+			this.categoryid = categoryid;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public List<Book> getBooks() {
+			return books;
+		}
+
+		public void setBooks(List<Book> books) {
+			this.books = books;
+		}
+
+		@Override
+		public String toString() {
+			return "Category [categoryid=" + categoryid + ", name=" + name + "]";
+		}
+	
+	
+	
+}
---
+++ src/main/java/fi/haagahelia/bookstore/domain/CategoryRepository.java
@@ -0,0 +1,12 @@
+package fi.haagahelia.bookstore.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+	List <Category> findByName(@Param("name") String name);
+}
